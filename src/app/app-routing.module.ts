bc9d292c2ef30511188e657a40a37bee0482a164
HR1Aventum <rohan.arya237@gmail.com>
2020-06-02 11:18:01 +0530

    ~

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { UserListComponent } from './user-list/user-list.component';
+import { UserCreateComponent } from './user-create/user-create.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
+const routes: Routes = [
+  {
+    path:"user-list",
+    component:UserListComponent
+  },
+  {
+    path:'user-create',
+    component:UserCreateComponent
+  },
+  {
+    path:'user-edit/:id',
+    component:UserEditComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
